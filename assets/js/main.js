9b026939ec8e5b554fcd38c3aa21386fa5f7a713
Harsh Vardhan <harsh1002@chitkara.edu.in>
2022-03-04 00:15:48 +0530

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,3 +120,14 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr=ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true
+
+})
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`,{delay:700})
+sr.reveal(`.home__social`,`.home__scroll`,{delay:900,origin:'bottom'})
